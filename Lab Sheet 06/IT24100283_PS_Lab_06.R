e3f875bfbb9c56a6d0dd6f22da89342fdf315537
IT24100283 <it24100283@my.sliit.lk>
2025-09-02 22:29:53 +0530

    Add files via upload

---
+++ Lab Sheet 06/IT24100283_PS_Lab_06.R
@@ -0,0 +1,16 @@
+setwd("C:\\Users\\vishw\\Downloads\\Lab Sheet 06")
+getwd()
+
+#1
+
+#i- Binomial Distribution
+#ii- 
+1-pbinom(46,50,0.85,lower.tail = TRUE)
+
+#2
+
+#i-Number of calls per hour
+#ii-Poisson distribution
+#iii-
+dpois(15,12)
+
